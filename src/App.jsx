8e749fe09f4e497d0d577f10c42cabb7b17967b5
MuneeburRehman156 <muneeb156651@gmail.com>
2026-02-21 16:45:53 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import PricingSection from "./components/Pricing Section/PricingSection";
+
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="app">
-      {/* Navbar stays on top */}
-      <Navbar />
+    // <div className="app">
+    //   {/* Navbar stays on top */}
+    //   <Navbar />
 
-      {/* Full width banner section (navbar sits above it) */}
-      <section className="heroWrap">
-        <div className="heroWrap__inner">
-          <Hero />
-        </div>
-      </section>
-    </div>
+    //   {/* Full width banner section (navbar sits above it) */}
+    //   <section className="heroWrap">
+    //     <div className="heroWrap__inner">
+    //       <Hero />
+    //     </div>
+    //   </section>
+    //         <PricingSection />
+
+    // </div>
+
+    <>
+    
+<Navbar/>
+
+<Hero/>
+
+<PricingSection/>
+
+ 
+    
+    
+    </>
   );
 }
